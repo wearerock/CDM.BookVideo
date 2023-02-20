a020ba8652ace80ca099af66a8258ac5aa866c43
Iegor Meshcheriakov <iegor.meshcheriakov@advancedmd.com>
2023-02-20 19:48:28 +0200

    Add comment

--- src/CDM.BookVideo.Application/Events/EventHandling/OrderPurchasedEventHandler.cs
+++ src/CDM.BookVideo.Application/Events/EventHandling/OrderPurchasedEventHandler.cs
@@ -1,6 +1,7 @@
 ﻿using CDM.BookVideo.EventBus.Events;
 
 namespace CDM.BookVideo.Application.Events.EventHandling {
+  // ACTUALLY HAS TO BE HANDLED IN SEPARATE SERVICE OR MAYBE BACKGROUND SERVICE
   public class OrderPurchasedEventHandler : IIntegrationEventHandler<OrderPurchasedEvent> {
     public Task Handle(OrderPurchasedEvent @event) {
       Console.WriteLine("Start delivering...");
